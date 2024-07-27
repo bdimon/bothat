518c14b48dcf79d2bc91299da9a8e9fcb0c4e5d0
Dmytro Baklan <dmytr0b@yahoo.com>
2024-07-27 18:09:15 +0200

    Initial commit

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+
+const TOKEN = '7260302057:AAEeRG2G2EVcvq1NVLl_rVDovnnHG_fLvbw';
+const BASE_URL = 'https://api.telegram.org/bot' . TOKEN . '/';
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'config.php';
+require_once 'functions.php';
+
+// false - returns "Object"
+// $res = json_decode(file_get_contents(BASE_URL . 'getMe'));
+// true - returns "Array"
+// $res = json_decode(file_get_contents(BASE_URL . 'getMe'), true);
+// $res = send_request('getUpdates');
+$updates = send_request('getUpdates',['offset' =>  540113303 + 1]);
+foreach($updates->result as $update) {
+    echo "{$update->update_id} - {$update->message->chat->id} - {$update->message->from->first_name} - {$update->message->text} </br>";
+}
+// debug($res->result->id);// for Object
+// debug($res['result']['id']); //for Array
+// debug($res);
